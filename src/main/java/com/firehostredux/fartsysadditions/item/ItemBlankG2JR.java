0443c8a1413c8ae290633cadb817d1e78e244bb3
Professor Fartsalot <professorfartsalot@gmail.com>
2023-11-14 03:36:18 -0500

    v1.3.0-4
    - Removed cronus recipe and unused advancements
    + Added blank jukebox roll with varying grades

---
+++ src/main/java/com/firehostredux/fartsysadditions/item/ItemBlankG2JR.java
@@ -0,0 +1,60 @@
+
+package com.firehostredux.fartsysadditions.item;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+import com.firehostredux.fartsysadditions.creativetab.TabFartsysAdditions;
+import com.firehostredux.fartsysadditions.ElementsFartsysmagitechfantasyMod;
+
+@ElementsFartsysmagitechfantasyMod.ModElement.Tag
+public class ItemBlankG2JR extends ElementsFartsysmagitechfantasyMod.ModElement {
+	@GameRegistry.ObjectHolder("fartsysmagitechfantasy:blank_g_2_jr")
+	public static final Item block = null;
+	public ItemBlankG2JR(ElementsFartsysmagitechfantasyMod instance) {
+		super(instance, 174);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("fartsysmagitechfantasy:blank_g_2_jr", "inventory"));
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 64;
+			setUnlocalizedName("blank_g_2_jr");
+			setRegistryName("blank_g_2_jr");
+			setCreativeTab(TabFartsysAdditions.tab);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+	}
+}
