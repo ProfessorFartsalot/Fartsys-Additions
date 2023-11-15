245fa4908566d9a8794f16873227c343a0f7b15f
Professor Fartsalot <professorfartsalot@gmail.com>
2023-11-15 02:43:33 -0500

    v1.3.1-4
    + Begin adding jukebox rolls for new music

---
+++ src/main/java/com/firehostredux/fartsysadditions/procedure/ProcedureJRLunacyRightClickedInAir.java
@@ -0,0 +1,35 @@
+package com.firehostredux.fartsysadditions.procedure;
+
+import net.minecraftforge.items.ItemHandlerHelper;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.Entity;
+
+import java.util.Map;
+
+import com.firehostredux.fartsysadditions.item.ItemJRLunacy;
+import com.firehostredux.fartsysadditions.item.ItemAMDLunacy;
+import com.firehostredux.fartsysadditions.ElementsFartsysmagitechfantasyMod;
+
+@ElementsFartsysmagitechfantasyMod.ModElement.Tag
+public class ProcedureJRLunacyRightClickedInAir extends ElementsFartsysmagitechfantasyMod.ModElement {
+	public ProcedureJRLunacyRightClickedInAir(ElementsFartsysmagitechfantasyMod instance) {
+		super(instance, 187);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			System.err.println("Failed to load dependency entity for procedure JRLunacyRightClickedInAir!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity instanceof EntityPlayer) {
+			ItemStack _setstack = new ItemStack(ItemAMDLunacy.block, (int) (1));
+			_setstack.setCount(1);
+			ItemHandlerHelper.giveItemToPlayer(((EntityPlayer) entity), _setstack);
+		}
+		if (entity instanceof EntityPlayer)
+			((EntityPlayer) entity).inventory.clearMatchingItems(new ItemStack(ItemJRLunacy.block, (int) (1)).getItem(), -1, (int) 1, null);
+	}
+}
