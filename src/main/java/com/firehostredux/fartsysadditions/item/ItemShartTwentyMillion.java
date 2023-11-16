bb5d8ac21a866aa301d9f9b1eb96f42bd898cbb4
Professor Fartsalot <professorfartsalot@gmail.com>
2023-11-15 20:33:30 -0500

    v1.3.3
    + Added variant sharts to finally patch out the infinite money bug.
    + Made sharts PROPERLY craftable and 1:1
    + Updated to v1.3.3

--- src/main/java/com/firehostredux/fartsysadditions/item/ItemShartTwentyMillion.java
+++ src/main/java/com/firehostredux/fartsysadditions/item/ItemShartTwentyMillion.java
@@ -10,26 +10,21 @@ import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraft.world.World;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
-import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.entity.ai.attributes.AttributeModifier;
-import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.block.state.IBlockState;
 
 import java.util.List;
 
-import com.google.common.collect.Multimap;
-
 import com.firehostredux.fartsysadditions.creativetab.TabFartsysAdditions;
 import com.firehostredux.fartsysadditions.ElementsFartsysmagitechfantasyMod;
 
 @ElementsFartsysmagitechfantasyMod.ModElement.Tag
-public class ItemShartThousand extends ElementsFartsysmagitechfantasyMod.ModElement {
-	@GameRegistry.ObjectHolder("fartsysmagitechfantasy:shart_thousand")
+public class ItemShartTwentyMillion extends ElementsFartsysmagitechfantasyMod.ModElement {
+	@GameRegistry.ObjectHolder("fartsysmagitechfantasy:shart_twenty_million")
 	public static final Item block = null;
-	public ItemShartThousand(ElementsFartsysmagitechfantasyMod instance) {
-		super(instance, 52);
+	public ItemShartTwentyMillion(ElementsFartsysmagitechfantasyMod instance) {
+		super(instance, 275);
 	}
 
 	@Override
@@ -40,14 +35,14 @@ public class ItemShartThousand extends ElementsFartsysmagitechfantasyMod.ModElem
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void registerModels(ModelRegistryEvent event) {
-		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("fartsysmagitechfantasy:shart_thousand", "inventory"));
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("fartsysmagitechfantasy:shart_twenty_million", "inventory"));
 	}
 	public static class ItemCustom extends Item {
 		public ItemCustom() {
 			setMaxDamage(0);
 			maxStackSize = 64;
-			setUnlocalizedName("shart_thousand");
-			setRegistryName("shart_thousand");
+			setUnlocalizedName("shart_twenty_million");
+			setRegistryName("shart_twenty_million");
 			setCreativeTab(TabFartsysAdditions.tab);
 		}
 
@@ -67,21 +62,16 @@ public class ItemShartThousand extends ElementsFartsysmagitechfantasyMod.ModElem
 		}
 
 		@Override
-		public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot slot) {
-			Multimap<String, AttributeModifier> multimap = super.getItemAttributeModifiers(slot);
-			if (slot == EntityEquipmentSlot.MAINHAND) {
-				multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(),
-						new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Item modifier", (double) -1, 0));
-				multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Item modifier", -2.4, 0));
-			}
-			return multimap;
+		@SideOnly(Side.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
 		}
 
 		@Override
 		public void addInformation(ItemStack itemstack, World world, List<String> list, ITooltipFlag flag) {
 			super.addInformation(itemstack, world, list, flag);
-			list.add("Finally");
-			list.add("one kilo shart per second!");
+			list.add("Twenty MILLION sharts. What are you EATING???? That's like");
+			list.add("a weapon of ass destruction! Oh wait-");
 		}
 	}
 }
--- src/main/java/com/firehostredux/fartsysadditions/item/ItemShartTwoThousand.java
+++ src/main/java/com/firehostredux/fartsysadditions/item/ItemShartTwoThousand.java
@@ -10,26 +10,21 @@ import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraft.world.World;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
-import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.entity.ai.attributes.AttributeModifier;
-import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.block.state.IBlockState;
 
 import java.util.List;
 
-import com.google.common.collect.Multimap;
-
 import com.firehostredux.fartsysadditions.creativetab.TabFartsysAdditions;
 import com.firehostredux.fartsysadditions.ElementsFartsysmagitechfantasyMod;
 
 @ElementsFartsysmagitechfantasyMod.ModElement.Tag
-public class ItemShartThousand extends ElementsFartsysmagitechfantasyMod.ModElement {
-	@GameRegistry.ObjectHolder("fartsysmagitechfantasy:shart_thousand")
+public class ItemShartTwoThousand extends ElementsFartsysmagitechfantasyMod.ModElement {
+	@GameRegistry.ObjectHolder("fartsysmagitechfantasy:shart_two_thousand")
 	public static final Item block = null;
-	public ItemShartThousand(ElementsFartsysmagitechfantasyMod instance) {
-		super(instance, 52);
+	public ItemShartTwoThousand(ElementsFartsysmagitechfantasyMod instance) {
+		super(instance, 271);
 	}
 
 	@Override
@@ -40,14 +35,14 @@ public class ItemShartThousand extends ElementsFartsysmagitechfantasyMod.ModElem
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void registerModels(ModelRegistryEvent event) {
-		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("fartsysmagitechfantasy:shart_thousand", "inventory"));
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("fartsysmagitechfantasy:shart_two_thousand", "inventory"));
 	}
 	public static class ItemCustom extends Item {
 		public ItemCustom() {
 			setMaxDamage(0);
 			maxStackSize = 64;
-			setUnlocalizedName("shart_thousand");
-			setRegistryName("shart_thousand");
+			setUnlocalizedName("shart_two_thousand");
+			setRegistryName("shart_two_thousand");
 			setCreativeTab(TabFartsysAdditions.tab);
 		}
 
@@ -67,21 +62,9 @@ public class ItemShartThousand extends ElementsFartsysmagitechfantasyMod.ModElem
 		}
 
 		@Override
-		public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot slot) {
-			Multimap<String, AttributeModifier> multimap = super.getItemAttributeModifiers(slot);
-			if (slot == EntityEquipmentSlot.MAINHAND) {
-				multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(),
-						new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Item modifier", (double) -1, 0));
-				multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Item modifier", -2.4, 0));
-			}
-			return multimap;
-		}
-
-		@Override
 		public void addInformation(ItemStack itemstack, World world, List<String> list, ITooltipFlag flag) {
 			super.addInformation(itemstack, world, list, flag);
-			list.add("Finally");
-			list.add("one kilo shart per second!");
+			list.add("TWO kilo sharts per second!");
 		}
 	}
 }
